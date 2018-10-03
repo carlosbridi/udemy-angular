ca5ecc0dbcd3f603269beb8f2b37fe0e2cf41e84
CarlosBridi <carlosbridi@gmail.com>
2018-10-02 23:13:01 -0300

    Criação do carrinho

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -11,6 +11,7 @@ import 'rxjs/add/operator/catch';
 
 import {MEAT_API} from '../app.api';
 import { Observable } from 'rxjs/Observable';
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService { 
@@ -26,6 +27,19 @@ export class RestaurantsService {
     restaurantById(id: string): Observable<Restaurant>{
       return this.http.get(`${MEAT_API}/restaurants/${id}`)
         .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any> {
+      return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+    }
+
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]>{
+      return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+      .map(response => response.json())
         .catch(ErrorHandler.handleError);
     }
 
